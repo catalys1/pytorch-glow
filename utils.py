7349aa98ebf144e0d979b2b392ad17d59b7964d4
Connor Anderson <thecatalystak@gmail.com>
2019-02-25 16:40:11 -0700

    Remove uneeded functions from utils.py

--- utils.py
+++ utils.py
@@ -13,36 +13,10 @@ import os
 # ------------------------------------------------------------------------------
 
 def flatten_sum(logps):
-    while len(logps.size()) > 1: 
-        logps = logps.sum(dim=-1)
-    # logps = logps.view(logps.shape[0], -1).sum(-1)
+    #while len(logps.size()) > 1: 
+    #    logps = logps.sum(dim=-1)
+    logps = logps.view(logps.shape[0], -1).sum(-1)
     return logps
-
-# ------------------------------------------------------------------------------
-# Logging
-# ------------------------------------------------------------------------------
-
-def save_session(model, optim, args, epoch):
-    path = os.path.join(args.save_dir, str(epoch))
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    # save the model and optimizer state
-    torch.save(model.state_dict(), os.path.join(path, 'model.pth'))
-    torch.save(optim.state_dict(), os.path.join(path, 'optim.pth'))
-    print('Successfully saved model')
-
-def load_session(model, optim, args):
-    try: 
-        start_epoch = int(args.load_dir.split('/')[-1])
-        model.load_state_dict(torch.load(os.path.join(args.load_dir, 'model.pth')))
-        optim.load_state_dict(torch.load(os.path.join(args.load_dir, 'optim.pth')))
-        print('Successfully loaded model')
-    except Exception as e:
-        pdb.set_trace()
-        print('Could not restore session properly')
-
-    return model, optim, start_epoch
 
 
 # ------------------------------------------------------------------------------
