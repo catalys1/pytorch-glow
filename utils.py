10d7d41993ab060fdc0a27f88fb8cf9dd905a0af
Connor Anderson <thecatalystak@gmail.com>
2019-02-25 17:04:45 -0700

    Add standard_normal function to utils.py

--- utils.py
+++ utils.py
@@ -8,9 +8,12 @@ import numpy as np
 import pdb
 import os
 
+
 # ------------------------------------------------------------------------------
 # Utility Methods
 # ------------------------------------------------------------------------------
+
+Log2PI = float(np.log(2 * np.pi))
 
 def flatten_sum(logps):
     #while len(logps.size()) > 1: 
@@ -23,9 +26,24 @@ def flatten_sum(logps):
 # Distributions
 # ------------------------------------------------------------------------------
 
+def standard_normal(shape, device='cuda'):
+    class Normal(object):
+        def logps(x):
+            return -0.5 * (Log2PI + x**2)
+
+        def sample():
+            s = torch.empty(shape, device=device).normal_(mean=0, std=1)
+
+        def logp(x):
+            flatten_sum(self.logps(x))
+
+    return Normal
+
+
 def standard_gaussian(shape):
     mean, logsd = [torch.cuda.FloatTensor(shape).fill_(0.) for _ in range(2)]
     return gaussian_diag(mean, logsd)
+
 
 def gaussian_diag(mean, logsd):
     class o(object):
